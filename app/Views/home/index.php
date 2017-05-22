cc021945ed739a35dbf07c8fe6401ce2db402d35
Pontus Nilsson <iceroad@neradio.com>
2017-05-22 18:24:50 +0200

    New toplist layout!

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,10 +1,30 @@
 
 <?php get_header(); ?>
-        <h1> The topplist </h1>
-        <ol>
-            <?php foreach($toplist as $movie) : ?>
-                <li><a href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/movie/show/<?php echo $movie->id; ?>"> <?php echo $movie->titel; ?></a> </li>
-            <?php endforeach; ?>
-        </ol>
+<style>
 
+</style>
+<h1 class="my-4 text-center"> 10 great movies to see while you are hangover </h1>
+<?php $i=1; foreach($toplist as $movie): ?>
+       <div class="toplist_box mb-3 border-1">
+            <div class="alert alert-success">
+                <span class="float-left h2">#<?php echo $i; ?></span>
+                <h4 class="text-center"><a class="h4" href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/movie/show/<?php echo $movie->id; ?>"><?php echo $movie->titel; ?></a></h4>
+            </div>
+            <a href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/movie/show/<?php echo $movie->id; ?>">
+                <img class="float-left mr-2" src="<?php echo $movie->bild; ?>">
+            </a>
+            <p><?php echo $movie->info; ?>
+                <div class="mt-5 text-center h2">
+                <?php for($j=1; $j<=5; $j++): ?>
+                    <?php if($j <= $movie->rate): ?>
+                        ★
+                    <?php else: ?>
+                        ☆
+                    <?php endif; ?>
+                <?php endfor; ?>
+                </div>
+            </p>
+            <div class="clearfix"></div>
+        </div>
+<?php $i++; endforeach; ?>
 <?php get_footer();?>
