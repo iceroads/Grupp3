2a5526b62703ea102b5fd4b003563488e0a0e38c
Ramin <macbookpro@macbookpros-MacBook-Pro.local>
2017-05-22 15:16:53 +0200

    no message

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -3,7 +3,7 @@
         <h1> The topplist </h1>
         <ol>
             <?php foreach($toplist as $movie) : ?>
-                <li><a href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/movie/<?php echo $movie->id; ?>"> <?php echo $movie->titel; ?></a> </li>
+                <li><a href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/movie/show/<?php echo $movie->id; ?>"> <?php echo $movie->titel; ?></a> </li>
             <?php endforeach; ?>
         </ol>
 
